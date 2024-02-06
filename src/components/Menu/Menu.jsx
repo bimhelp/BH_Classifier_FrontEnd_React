30ed06d8aa0577e1a586ed074ded0cfb1ce9c1f1
Alex-Sorokolit <sorokolital@gmail.com>
2024-02-06 15:38:43 +0200

    menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserMenu from "../UserMenu/UserMenu";
+import { authContext as context } from "../../context/authContext";
 // Навігація по сайту
 import { Navigation, MenuWrapper, StyledNavLink } from "./Menu.styled";
 
 const Menu = () => {
+  const { isLoggedIn } = useContext(context);
+
   return (
     <Navigation>
       <MenuWrapper>
         <StyledNavLink to="/">CPV</StyledNavLink>
-        <StyledNavLink to="projects">Projects</StyledNavLink>
-        <StyledNavLink to="add">Add</StyledNavLink>
+        {isLoggedIn && (
+          <>
+            <StyledNavLink to="projects">Projects</StyledNavLink>
+            <StyledNavLink to="add">Add</StyledNavLink>
+          </>
+        )}
       </MenuWrapper>
       <UserMenu />
     </Navigation>
