e93d8da5d55298a16c3e5c81b1a8000fcd6b7378
Alex-Sorokolit <sorokolital@gmail.com>
2024-03-27 07:53:21 +0200

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,23 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import { CustomLoader } from "./Loader.styled";
 import ContentLoader from "react-content-loader";
+
 const Loader = () => {
-  return (
-    <ContentLoader
-      speed={2}
-      width={1415}
-      height={78}
-      viewBox="0 0 1415 78"
-      backgroundColor="#e3e3e3"
-      foregroundColor="#c7c7c7"
-    >
-      <rect x="15" y="0" rx="5" ry="5" width="110" height="24" />
-      <rect x="130" y="0" rx="5" ry="5" width="1285" height="24" />
-      <rect x="15" y="27" rx="5" ry="5" width="110" height="24" />
-      <rect x="130" y="27" rx="5" ry="5" width="1285" height="24" />
-      <rect x="15" y="54" rx="5" ry="5" width="110" height="24" />
-      <rect x="130" y="54" rx="5" ry="5" width="1285" height="24" />
-    </ContentLoader>
-  );
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Під час відключення компонента, видаляємо обробник події
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Пустий масив означає, що ефект виконується лише після монтування компонента
+
+  // Функція для визначення, яка розмітка відображається залежно від ширини екрану
+  const getLayout = () => {
+    if (windowWidth < 600) {
+      return (
+        <>
+          {/* Розмітка для малих екранів */}
+          <ContentLoader
+            speed={2}
+            width={550}
+            height={32}
+            viewBox="0 0 550 32"
+            backgroundColor="#e3e3e3"
+            foregroundColor="#c7c7c7"
+            style={{ width: "100%" }}
+          >
+            <rect x="10" y="0" rx="5" ry="5" width="550" height="32" />
+          </ContentLoader>
+        </>
+      );
+    } else {
+      return (
+        <>
+          {/* Розмітка для великих екранів */}
+          <ContentLoader
+            speed={2}
+            width={1415}
+            height={32}
+            viewBox="0 0 1415 32"
+            backgroundColor="#e3e3e3"
+            foregroundColor="#c7c7c7"
+            style={{ width: "100%" }}
+          >
+            <rect x="15" y="0" rx="5" ry="5" width="110" height="32" />
+            <rect x="130" y="0" rx="5" ry="5" width="1285" height="32" />
+          </ContentLoader>
+        </>
+      );
+    }
+  };
+
+  return <>{getLayout()}</>;
 };
 
 export default Loader;
+
+// import React from "react";
+// import { CustomLoader } from "./Loader.styled";
+// const Loader = () => {
+//   return (
+//     <CustomLoader
+//       speed={2}
+//       width={1200}
+//       height={40}
+//       // viewBox="0 0 1415 78"
+//       viewBox="0 0 1200 37"
+//       backgroundColor="#e3e3e3"
+//       foregroundColor="#c7c7c7"
+//       // style={{ width: "100%" }}
+//     >
+//       <rect x="15" y="0" rx="5" ry="5" width="110" height="32" />
+//       <rect x="130" y="0" rx="5" ry="5" width="1000" height="32" />
+//       {/* <rect x="15" y="27" rx="5" ry="5" width="110" height="24" />
+//       <rect x="130" y="27" rx="5" ry="5" width="1285" height="24" />
+//       <rect x="15" y="54" rx="5" ry="5" width="110" height="24" />
+//       <rect x="130" y="54" rx="5" ry="5" width="1285" height="24" /> */}
+//     </CustomLoader>
+//   );
+// };
+
+// export default Loader;
---
+++ src/components/Loader/Loader.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import ContentLoader from "react-content-loader";
+
+export const CustomLoader = styled(ContentLoader)`
+  width: 100%;
+  @media screen and (min-width: 480px) {
+    width: 100%;
+  }
+`;
