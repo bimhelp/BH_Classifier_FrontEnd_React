39ee3901762de24646572325cd6716b970014b24
Alex-Sorokolit <sorokolital@gmail.com>
2024-04-18 15:01:57 +0300

    Updates

---
+++ static/js/657.6f13ba59.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclassifier=self.webpackChunkclassifier||[]).push([[657],{657:function(t,c,i){i.d(c,{x3W:function(){return e}});var a=i(2119);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M7.75 6h8.5c.966 0 1.75.784 1.75 1.75v8.5A1.75 1.75 0 0 1 16.25 18h-8.5A1.75 1.75 0 0 1 6 16.25v-8.5C6 6.784 6.784 6 7.75 6Z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=657.6f13ba59.chunk.js.map
