930370ed9d0477fad65b4d9f04d174af4de57201
Alex-Sorokolit <sorokolital@gmail.com>
2025-01-22 13:43:29 +0200

    back to user services

--- src/pages/UserServicePage/UserServicePage.jsx
+++ src/pages/UserServicePage/UserServicePage.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Section from "../../components/Section/Section";
-import { getServiceByUser } from "../../services";
+import { getServiceByUser, getServiceById } from "../../services";
 import { toast } from "react-toastify";
 import { BarLoader } from "react-spinners";
 import ServiceList from "../../components/ServiceList/ServiceList";
+import { Menu, Content, Layout } from "./UserServicePage.styled";
+import { IconButton } from "../../components/Button/Button";
+import { IoMdBackspace } from "react-icons/io";
 
 const UserServicePage = () => {
   const [userServices, setUserServices] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [status, setStatus] = useState("userMaterials");
+  const [service, setService] = useState([]);
 
   // Запит по матеріали користувача
   useEffect(() => {
@@ -26,22 +31,69 @@ const UserServicePage = () => {
     }
     userServices();
   }, []);
+
+  // Отримати по id
+
+  const serviceById = (id) => {
+    setStatus("service");
+    const controller = new AbortController();
+    async function getService(id) {
+      try {
+        const response = await getServiceById(id, controller.signal);
+        setService([response.data]);
+      } catch {
+        toast.error("Не вдалось отримати послугу");
+      }
+    }
+    getService(id);
+    return () => {
+      controller.abort();
+    };
+  };
+
+  const backToUserServices = () => {
+    setStatus("userServices");
+  };
+
   return (
     <>
       <Section>
-        <p>Мої послуги:</p>
-        {isLoading ? (
-          <BarLoader color="#125b56" width="100%" />
-        ) : (
-          <Section>
-            {userServices.length < 1 && (
-              <p>
-                Тут будуть відображатись ваші послуги, створіть першу послугу
-              </p>
+        <Menu>
+          {status === "service" && (
+            <IconButton
+              onClick={backToUserServices}
+              variant="dark"
+              tooltip="Повернутись"
+              icon={IoMdBackspace}
+              iconSize="40px"
+            ></IconButton>
+          )}
+        </Menu>
+        <Layout>
+          <Content>
+            <p>Мої послуги:</p>
+            {isLoading ? (
+              <BarLoader color="#125b56" width="100%" />
+            ) : (
+              <Section>
+                {userServices.length < 1 && (
+                  <p>
+                    Тут будуть відображатись ваші послуги, створіть першу
+                    послугу
+                  </p>
+                )}
+
+                {status === "userServices" && (
+                  <ServiceList items={userServices} byId={serviceById} />
+                )}
+
+                {status === "service" && (
+                  <ServiceList items={service} byId={serviceById} />
+                )}
+              </Section>
             )}
-            <ServiceList items={userServices} />
-          </Section>
-        )}
+          </Content>
+        </Layout>
       </Section>
     </>
   );
---
+++ src/pages/UserServicePage/UserServicePage.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  display: flex;
+  width: 100%;
+
+  /* outline: 1px solid green; */
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  /* outline: 1px solid red; */
+`;
+export const Menu = styled.div`
+  margin-top: 20px;
+  display: flex;
+`;
