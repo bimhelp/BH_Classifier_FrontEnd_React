7ee3acfd67fbbcf7e321af101900521378c451ef
Alex-Sorokolit <sorokolital@gmail.com>
2025-06-05 11:28:13 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import { ThemeProvider } from "styled-components";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/BH_Classifier_FrontEnd_React">
+    <BrowserRouter basename="/bimhelp-classifier.online">
       <ThemeProvider theme={theme}>
         <AuthProvider>
           <MaterialProvider>
