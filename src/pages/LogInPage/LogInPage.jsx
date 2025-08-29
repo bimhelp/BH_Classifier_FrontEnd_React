3c7669f92a4dccfc8629b6751f808c6f7f0b77ca
Alex-Sorokolit <sorokolital@gmail.com>
2025-08-29 16:53:47 +0300

    google off temporary

--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Section from "../../components/Section/Section";
 import LoginForm from "../../components/LoginForm/LoginForm";
-import GoogleLoginBtn from "../../components/GoogleLoginBtn/GoogleLoginBtn";
+// import GoogleLoginBtn from "../../components/GoogleLoginBtn/GoogleLoginBtn";
 
 const LogInPage = () => {
   return (
